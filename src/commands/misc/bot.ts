d0a758bb29fb4d61ff20fb096b1596cb958b203f
flufl <flufftehgamer@gmail.com>
2022-09-23 12:48:35 +0700

    📝 | Whitespace

--- src/commands/misc/bot.ts
+++ src/commands/misc/bot.ts
@@ -99,9 +99,11 @@ export default new Command({
 
             case 'uptime': {
                 let totalSeconds = (client.uptime / 1000);
-                totalSeconds %= 86400;
+                totalSeconds %= 86400
+
                 let hours = Math.floor(totalSeconds / 3600);
                 totalSeconds %= 3600;
+                
                 let minutes = Math.floor(totalSeconds / 60);
                 let seconds = Math.floor(totalSeconds % 60);
 
