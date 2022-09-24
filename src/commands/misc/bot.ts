559c522bea6c3de9e582ee780dd5f0950d1ca589
flufl <flufftehgamer@gmail.com>
2022-09-24 18:23:19 +0700

    📝 | Added `/stats`

--- src/commands/misc/bot.ts
+++ src/commands/misc/bot.ts
@@ -13,6 +13,11 @@ export default new Command({
             type: 'SUB_COMMAND'
         },
         {
+            name: 'stats',
+            description: 'Display bot\'s stats.',
+            type: 'SUB_COMMAND'
+        },
+        {
             name: 'uptime',
             description: 'Show the bot\'s uptime.',
             type:'SUB_COMMAND'
@@ -78,6 +83,34 @@ export default new Command({
                             .setURL('https://discordservices.net/bot/1002193298229829682'),
                     )
                 await interaction.reply({embeds:[shinanoEmbed], components: [buttons1, buttons2]})
+                break
+            }
+
+            case 'stats': {
+                await interaction.deferReply()
+                let totalSeconds = (client.uptime / 1000);
+                totalSeconds %= 86400
+
+                let hours = Math.floor(totalSeconds / 3600);
+                totalSeconds %= 3600;
+                
+                let minutes = Math.floor(totalSeconds / 60);
+                let seconds = Math.floor(totalSeconds % 60);
+
+                const memory = process.memoryUsage()
+                const performance: MessageEmbed = new MessageEmbed()
+                    .setColor('BLUE')
+                    .setTitle('Shinano\'s Stats')
+                    .addFields(
+                        {name: 'Uptime:', value: `${hours} hours, ${minutes} minutes, ${seconds} seconds`},
+                        {name: 'Latency:', value: `Latency: ${Date.now() - interaction.createdTimestamp}ms\nAPI Latency: ${Math.round(client.ws.ping)}ms`},
+                        {name: 'Memory Usage:', value: 
+                        `RSS: ${(memory.rss / 1024**2).toFixed(2)} MB\n` +
+                        `External: ${(memory.external / 1024**2).toFixed(2)} MB\n` +
+                        `Heap Total Mem: ${(memory.heapTotal / 1024**2).toFixed(2)} MB\n` +
+                        `Heap Total Used: ${(memory.heapUsed / 1024**2).toFixed(2)} MB`}
+                    )
+                await interaction.editReply({embeds: [performance]})
                 break
             }
 
