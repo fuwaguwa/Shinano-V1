fb9b3e88b88a6a89ec1d6fdb4749aca08e329ea8
flufl <flufftehgamer@gmail.com>
2022-10-10 19:08:03 +0700

    📝 | Added server info

--- src/commands/misc/dev-scmds/usage.ts
+++ src/commands/misc/dev-scmds/usage.ts
@@ -1,17 +1,21 @@
 import { MessageEmbed } from "discord.js";
 import { ShinanoInteraction } from "../../../typings/Command";
+import os from 'os'
 
 export async function devUsage(interaction: ShinanoInteraction) {
     const memory = process.memoryUsage()
     const performance: MessageEmbed = new MessageEmbed()
         .setColor('BLUE')
-        .setTitle('Stats')
+        .setTitle('Memory Stats')
         .addFields(
-            {name: 'Node', value: `
-            RSS: **${(memory.rss / 1024**2).toFixed(2)} MB**
-            External: **${(memory.external / 1024**2).toFixed(2)} MB**
-            Heap Total Mem: **${(memory.heapTotal / 1024**2).toFixed(2)} MB**
-            Heap Total Used: **${(memory.heapUsed / 1024**2).toFixed(2)} MB**`}
+            {name: 'Memory Usage:', value: 
+            `RSS: ${(memory.rss / 1024**2).toFixed(2)}MB\n` +
+            `External: ${(memory.external / 1024**2).toFixed(2)}MB\n` +
+            `Heap Total Used: ${(memory.heapUsed / 1024**2).toFixed(2)}MB\n` +
+            `Heap Total Mem: ${(memory.heapTotal / 1024**2).toFixed(2)}MB`},
+            {name: 'Server Information:', value: 
+            `Free Mem: ${(os.freemem() / 1024**2).toFixed(2)}MB\n` +
+            `Total Mem: ${(os.totalmem() / 1024**2).toFixed(2)}MB`}
         )
     await interaction.editReply({embeds: [performance]})
 }
