5dde15ab6905e50dee486c72425bf1af5c454cd0
flufl <flufftehgamer@gmail.com>
2022-10-10 19:07:31 +0700

    📝 | Added user count status

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -14,6 +14,14 @@ export default new Event("ready", async () => {
     // Activities
     client.user.setStatus('online')
 
+
+    let shikikansCount = 0
+    client.guilds.cache.forEach(guild => {
+        shikikansCount += guild.memberCount
+    })
+
+
+
     const activitiesList: any = [
         {type: 'PLAYING', message: 'with Laffey'},
         {type: 'WATCHING', message: 'Akagi and Kaga fight'},
@@ -21,8 +29,10 @@ export default new Event("ready", async () => {
         {type: 'WATCHING', message: 'people typing /help'},
         {type: 'LISTENING', message: 'Sandy\'s singing'},
         {type: 'WATCHING', message: 'thighs pics'},
-        {type: 'WATCHING', message: 'you'}
+        {type: 'WATCHING', message: 'you'},
+        {type: 'WATCHING', message: `${shikikansCount.toLocaleString("en-US")} shikikans`}
     ]
+    
     setInterval(() => {
         const presence = activitiesList[Math.floor(Math.random() * activitiesList.length)]
         client.user.setActivity(presence.message, {type: presence.type})
