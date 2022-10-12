e3077412d4fd1acb8653aa0d99d6656d7e193e80
flufl <flufftehgamer@gmail.com>
2022-10-12 08:46:49 +0700

    📝 | Added `/dog`

---
+++ src/commands/image/dog.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from 'discord.js'
+import fetch from 'node-fetch'
+import { Command } from '../../structures/Command'
+
+
+export default new Command({
+    name: 'dog',
+    description: 'Generate a picture of a dog',
+    cooldown: 4500,
+    run: async({interaction}) => {
+        await interaction.deferReply()
+
+        const response = await fetch('https://api.thedogapi.com/v1/images/search', {method: "GET"})
+        const dog = await response.json()
+
+        const dogEmbed: MessageEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setImage(dog[0].url)
+            .setFooter({
+                text: `Requested by ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL({dynamic: true})
+            })
+        
+        await interaction.editReply({embeds: [dogEmbed]})
+    }
+})
