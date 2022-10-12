96568bd10498a1d8a61a688d00127fd6d45b0659
flufl <flufftehgamer@gmail.com>
2022-10-12 08:47:00 +0700

    📝 | Added `/cat`

---
+++ src/commands/image/cat.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from 'discord.js'
+import fetch from 'node-fetch'
+import { Command } from '../../structures/Command'
+
+
+export default new Command({
+    name: 'cat',
+    description: 'Generate a picture of a cat',
+    cooldown: 4500,
+    run: async({interaction}) => {
+        await interaction.deferReply()
+
+        const response = await fetch('https://api.thecatapi.com/v1/images/search', {method: 'GET'})
+        const cat = await response.json()
+
+        const catEmbed: MessageEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setImage(cat[0].url)
+            .setFooter({
+                text: `Requested by ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL({dynamic: true})
+            })
+        
+        await interaction.editReply({embeds: [catEmbed]})
+    }
+})
