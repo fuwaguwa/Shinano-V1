53a1cd80f705bac94650f771dbe75e6eb89e8bfb
flufl <flufftehgamer@gmail.com>
2022-10-19 00:14:45 +0700

    📝 | Auto-update shikikans count

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -12,20 +12,19 @@ export default new Event("ready", async () => {
 
     // Activities
     client.user.setStatus('online')
-
-    let shikikansCount = client.users.cache.size
-
-    const activitiesList: any = [
-        {type: 'PLAYING', message: 'with Laffey'},
-        {type: 'WATCHING', message: 'Akagi and Kaga fight'},
-        {type: 'WATCHING', message: 'the shikikan'},
-        {type: 'LISTENING', message: 'Sandy\'s singing'},
-        {type: 'PLAYING', message: 'with a supercar'},
-        {type: 'PLAYING', message: 'Azur Lane'},
-        {type: 'WATCHING', message: `${shikikansCount.toLocaleString("en-US")} shikikans`}
-    ]
     
     setInterval(() => {
+        let shikikansCount = client.users.cache.size
+
+        const activitiesList: any = [
+            {type: 'PLAYING', message: 'with Laffey'},
+            {type: 'WATCHING', message: 'Akagi and Kaga fight'},
+            {type: 'WATCHING', message: 'the shikikan'},
+            {type: 'LISTENING', message: 'Sandy\'s singing'},
+            {type: 'PLAYING', message: 'with a supercar'},
+            {type: 'PLAYING', message: 'Azur Lane'},
+            {type: 'WATCHING', message: `${shikikansCount.toLocaleString("en-US")} shikikans`}
+        ]
         const presence = activitiesList[Math.floor(Math.random() * activitiesList.length)]
         client.user.setActivity(presence.message, {type: presence.type})
     }, 30000)
