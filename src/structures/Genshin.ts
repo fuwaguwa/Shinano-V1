e2005be0bc6f853d101b50332fe764eb586b607a
flufl <flufftehgamer@gmail.com>
2022-10-19 21:55:52 +0700

    📝 | No more elemental type

--- src/structures/Genshin.ts
+++ src/structures/Genshin.ts
@@ -1,9 +1,34 @@
 import genshin from 'genshin-db'
-import { Element } from '../typings/Genshin'
+
+const elementColors = {
+    "Pyro": "#b7242a",
+    "Hydro": "#248fbd",
+    "Anemo": "#2a9d90",
+    "Electro": "#7553c3",
+    "Dendro": "#6cae22",
+    "Cryo": "#7ba6db",
+    "Geo": "#e5a659"
+}
+
+const elementIcons = {
+    "Pyro": "<:pyro:1003213063199133759>",
+    "Hydro": "<:hydro:1003213061575954442>",
+    "Anemo": "<:anemo:1003213059394895922>",
+    "Electro": "<:electro:1003213057046102037>",
+    "Dendro": "<:dendro:1003213054634377216>",
+    "Cryo": "<:cryo:1003213052579164200>",
+    "Geo": "<:geo:1003213050561699897>"
+}  
 
 const stars = (item: genshin.Character | genshin.Weapon) => {return "⭐".repeat(parseInt(item.rarity, 10))}
-const color = (char: genshin.Character, elementColors: Element) => {return elementColors[char.element]}
-const icon = (char: genshin.Character, elementIcons: Element) => {return elementIcons[char.element]}
+const color = (char: genshin.Character | string) => {
+    if (typeof char == 'string') return elementColors[char]
+    return elementColors[char.element]
+}
+const icon = (char: genshin.Character | string) => {
+    if (typeof char == 'string') return elementIcons[char]
+    return elementIcons[char.element]
+}
 
 
 export {stars, color, icon}
--- src/typings/Genshin.ts
+++
@@ -1,9 +0,0 @@
-export type Element = {
-    Pyro: string;
-    Hydro: string;
-    Anemo: string;
-    Electro: string;
-    Dendro: string;
-    Cryo: string;
-    Geo: string;
-}
