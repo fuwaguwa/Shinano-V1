8bfc094af6c82faf9e80d6cdcfa189bf1d51dc72
flufl <flufftehgamer@gmail.com>
2022-10-26 20:08:08 +0700

    📝 | Inclusion of mp4

--- src/commands/bonk/subcommands/nsfw/privateColle.ts
+++ src/commands/bonk/subcommands/nsfw/privateColle.ts
@@ -21,5 +21,6 @@ export async function nsfwPrivateCollection(interaction: ShinanoInteraction, lew
         if (image.link.includes('_ellieAL_') && interaction.channelId === '1020991018507698206') return interaction.editReply({content: '<@557567996890185730>,',embeds: [lewdEmbed], components: [imageLink]})
         return interaction.editReply({embeds: [lewdEmbed], components: [imageLink]})
     }
+    if (image.link.includes('_ellieAL_') && interaction.channelId === '1020991018507698206') return interaction.editReply({content: `<@557567996890185730>,\n${image.link}`})
     return interaction.editReply({content: image.link})
 }
