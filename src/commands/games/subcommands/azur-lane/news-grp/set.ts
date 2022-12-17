2e4d51016d5a68398cf59a353d7f1e12e7a9efbe
flufl <flufftehgamer@gmail.com>
2022-12-17 15:26:13 +0700

    📝 | Splitted the `/news` command

---
+++ src/commands/games/subcommands/azur-lane/news-grp/set.ts
@@ -0,0 +1,38 @@
+import { MessageEmbed, TextChannel } from "discord.js";
+import { ShinanoInteraction } from "../../../../../typings/Command";
+import News from '../../../../../schemas/ALNews'
+
+export async function azurLaneNewsSet(interaction: ShinanoInteraction) {
+    // Permission Check
+    await interaction.deferReply()
+
+    const guildUserPerms = (await interaction.guild.members.fetch(interaction.user)).permissions
+    if (!guildUserPerms.has('ADMINISTRATOR') && !guildUserPerms.has('MANAGE_WEBHOOKS')) {
+        const noPerm: MessageEmbed = new MessageEmbed()
+            .setColor('RED')
+            .setDescription('❌ | You need `Manage Webhooks` permission to use this command!')
+        return interaction.editReply({embeds: [noPerm]})
+    }
+
+
+    const channel = interaction.options.getChannel('channel') || interaction.channel
+    if (!interaction.guild.me.permissionsIn(channel as TextChannel).has("SEND_MESSAGES")) {
+        const noPerm: MessageEmbed = new MessageEmbed()
+            .setColor('RED')
+            .setDescription('❌ | Shinano does not have permission to send message in this channel')
+        return interaction.reply({embeds: [noPerm]})
+    }
+
+    
+    // Main
+    const dbChannel = await News.findOne({guildId: interaction.guild.id})
+    dbChannel
+        ? await News.findOneAndUpdate({guildId: interaction.guild.id}, {channelId: channel.id})
+        : await News.create({guildId: interaction.guild.id, channelId: channel.id})
+    const done: MessageEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setDescription(
+            `✅ | Shinano will now send the latest news/tweets about the game in <#${channel.id}>`
+        )
+    await interaction.editReply({embeds: [done]})
+}
