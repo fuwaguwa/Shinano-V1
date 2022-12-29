1c55ac83d359f4219d80002e72e92df8c7fbb147
flufl <flufftehgamer@gmail.com>
2022-12-29 22:54:18 +0700

    ➕ | Added `/baka`

---
+++ src/commands/reactions/baka.ts
@@ -0,0 +1,18 @@
+import { MessageEmbed } from 'discord.js'
+import { Command } from '../../structures/Command'
+import neko from 'nekos-fun'
+
+export default new Command({
+    name: 'baka',
+    description: 'Y-You idiot!',
+    cooldown: 4500,
+    run: async({interaction}) => {
+        await interaction.deferReply()
+
+        const bakaEmbed: MessageEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setImage(await neko.sfw.baka())
+        
+        await interaction.editReply({embeds: [bakaEmbed]})
+    }
+})
